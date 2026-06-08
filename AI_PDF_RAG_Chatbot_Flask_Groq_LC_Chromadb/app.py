3f55cc409d3ab084f4c99514fdf60639f8e133ab
akshayparab2016 <30803600+akshayparab2016@users.noreply.github.com>
2026-06-08 16:17:23 +0530

    Update app.py

--- AI_PDF_RAG_Chatbot_Flask_Groq_LC_Chromadb/app.py
+++ AI_PDF_RAG_Chatbot_Flask_Groq_LC_Chromadb/app.py
@@ -206,5 +206,5 @@ def ask_question():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
